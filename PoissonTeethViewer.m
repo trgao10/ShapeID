155dfa61bdac790d674f1e02d384f94138f011df
Tingran Gao <trgao10@math.duke.edu>
2015-05-06 11:20:55 -0400

    added PoissonTeethViewer

---
+++ PoissonTeethViewer.m
@@ -0,0 +1,24 @@
+%% preparation
+close all;
+path(pathdef);
+addpath(path,genpath([pwd '/utils/']));
+
+%% .off mesh path
+GroupName = 'Adapis';
+PoissonTeethPath = ['./meshes/PoissonTeeth/' GroupName '/off/'];
+
+%% read in all meshes
+numRandMeshes = 10;
+
+MeshList = cell(numRandMeshes,1);
+
+cback = 0;
+for j=1:numRandMeshes
+    MeshList{j} = Mesh('off', [PoissonTeethPath GroupName '_' sprintf('%02d', j) '.off']);
+    for cc=1:cback
+        fprintf('\b');
+    end
+    cback = fprintf(['%4d/' num2str(numRandMeshes) ' done.\n'], j);
+end
+
+drawMeshList(MeshList, struct('DisplayLayout',[2,5],'linkCamera','on','DisplayOrient','Horizontal'));
